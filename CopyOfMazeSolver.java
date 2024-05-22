74bcbfa3ab6439eb1789edbab07b233679655a46
akshitasantra <106004567+akshitasantra@users.noreply.github.com>
2024-05-22 00:55:44 -0500

    Update CopyOfMazeSolver.java

--- CopyOfMazeSolver.java
+++ CopyOfMazeSolver.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-public class CopyOfMazeSolver {
+public class MazeSolver {
     public static void main(String[] args) {
         List<Point> maze = readMaze("Maze1.txt");
         System.out.println(maze);
